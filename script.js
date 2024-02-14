68cd7d956dbfc6c378e46913a9f28c6909d9b45f
angelfp30 <156565466+angelfp30@users.noreply.github.com>
2024-02-14 01:10:36 -0500

    Added the dialog and some basic JS to make it work (and CSS styles, it looks pretty now)

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const dialog = document.querySelector('dialog');
+const newBook = document.querySelector('.add');
+const closeDialog = document.querySelector('.close-dialog')
+const bookName = document.querySelector('#book-title');
+const author = document.querySelector('#author-name');
+const pages = document.querySelector('#num-pages');
+const readStatus = document.querySelector('#read');
+
+
 const myLibrary = [];
 
 function Book() {
@@ -7,3 +16,14 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+newBook.addEventListener("click", () =>{
+    dialog.showModal();
+})
+closeDialog.addEventListener("click", () =>{
+    bookName.value = '';
+    author.value = '';
+    pages.value = '';
+    readStatus.checked = false;
+    dialog.close();
+})
