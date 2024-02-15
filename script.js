49e964100c2ad898f3fd27c298bc9bc39d98049a
angelfp30 <156565466+angelfp30@users.noreply.github.com>
2024-02-15 15:48:55 -0500

    The "Add book" button works and the bugs about the cancel button inside the dialog were fixed

--- script.js
+++ script.js
@@ -5,25 +5,69 @@ const bookName = document.querySelector('#book-title');
 const author = document.querySelector('#author-name');
 const pages = document.querySelector('#num-pages');
 const readStatus = document.querySelector('#read');
-
-
+const getBookData = document.querySelector('.confirm');
+const content = document.querySelector('.content');
+//array to store the books
 const myLibrary = [];
 
-function Book() {
+function Book(bookTitle, authorName, numPages, read) {
   // the constructor...
+    this.bookTitle = bookTitle;
+    this.authorName = authorName;
+    this.numPages = numPages;
+    this.read = read;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  // doing stuff here
+    const book = new Book(
+        bookName.value,
+        author.value,
+        pages.value,
+        readStatus.checked ? "Read" : "Not read"
+        );
+    myLibrary.push(book);
+    
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+        
+        // Add book details to the card
+        card.innerHTML = `
+            <div class="book-name">Book: ${book.bookTitle}</div>
+            <div class="author">Author: ${book.authorName}</div>
+            <div class="pages">Pages: ${book.numPages}</div>
+            <div class="read-status">${book.read}</div>
+            <button class="delete">Delete</button>
+        `;
+        
+        // Append the card to the content container
+        content.appendChild(card);
+        clearValues();
 }
 
 newBook.addEventListener("click", () =>{
     dialog.showModal();
 })
+getBookData.addEventListener("click", (event) =>{
+    event.preventDefault();
+    dialog.close(
+        bookName.value,
+        author.value,
+        pages.value,
+        readStatus.checked ? "Read" : "Not read");
+})
+dialog.addEventListener("close", (e) =>{
+    dialog.returnValue === "" ? clearValues() : addBookToLibrary();
+})
+
 closeDialog.addEventListener("click", () =>{
+    dialog.returnValue = "";
+    clearValues();
+})
+function clearValues() {
     bookName.value = '';
     author.value = '';
     pages.value = '';
     readStatus.checked = false;
     dialog.close();
-})
+}
