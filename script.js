d6f306a20bc0ad1d710615efe3a6c48253410e5a
angelfp30 <156565466+angelfp30@users.noreply.github.com>
2024-02-15 23:03:56 -0500

    The previous toggle button was bugging some functions, everything works

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const pages = document.querySelector('#num-pages');
 const readStatus = document.querySelector('#read');
 const getBookData = document.querySelector('.confirm');
 const content = document.querySelector('.content');
-//array to store the books
+const deleteCard = document.querySelector('.delete')
+const cardReadStatus = document.querySelector('.read-or-not-read');
+//Array to store the books
 const myLibrary = [];
 
 function Book(bookTitle, authorName, numPages, read) {
@@ -19,51 +21,35 @@ function Book(bookTitle, authorName, numPages, read) {
 }
 
 function addBookToLibrary() {
-  // doing stuff here
+  // Entering the values from the form into the object
     const book = new Book(
         bookName.value,
         author.value,
         pages.value,
-        readStatus.checked ? "Read" : "Not read"
+        readStatus.checked ? "Read " : "Not read "
         );
     myLibrary.push(book);
     
-        const card = document.createElement('div');
-        card.setAttribute('class', 'card');
+        const newCard = document.createElement('div');
+        newCard.setAttribute('class', 'card');
         
-        // Add book details to the card
-        card.innerHTML = `
+        // Add book data to the card with HTML
+        newCard.innerHTML = `
             <div class="book-name">Book: ${book.bookTitle}</div>
             <div class="author">Author: ${book.authorName}</div>
             <div class="pages">Pages: ${book.numPages}</div>
-            <div class="read-status">${book.read}</div>
+            <div class="read-status">
+                <div class="read-or-not-read">${book.read}</div>
+                <input type="checkbox" class="check" id="check">
+            </div>
             <button class="delete">Delete</button>
         `;
         
         // Append the card to the content container
-        content.appendChild(card);
+        content.appendChild(newCard);
         clearValues();
 }
-
-newBook.addEventListener("click", () =>{
-    dialog.showModal();
-})
-getBookData.addEventListener("click", (event) =>{
-    event.preventDefault();
-    dialog.close(
-        bookName.value,
-        author.value,
-        pages.value,
-        readStatus.checked ? "Read" : "Not read");
-})
-dialog.addEventListener("close", (e) =>{
-    dialog.returnValue === "" ? clearValues() : addBookToLibrary();
-})
-
-closeDialog.addEventListener("click", () =>{
-    dialog.returnValue = "";
-    clearValues();
-})
+//Clearing the form after closing it or submitting the data
 function clearValues() {
     bookName.value = '';
     author.value = '';
@@ -71,3 +57,41 @@ function clearValues() {
     readStatus.checked = false;
     dialog.close();
 }
+newBook.addEventListener("click", () =>{
+    dialog.showModal();
+})
+getBookData.addEventListener("click", (event) =>{
+    event.preventDefault();
+    //The dialog closes but it submits the values from the form
+    dialog.close(
+        bookName.value,
+        author.value,
+        pages.value,
+        readStatus.checked ? "Read" : "Not read");
+})
+//If the form is empty a card is not created
+dialog.addEventListener("close", (e) =>{
+    dialog.returnValue === "" ? clearValues() : addBookToLibrary();
+})
+//If the close button is clicked, it clears the whole data
+closeDialog.addEventListener("click", () =>{
+    dialog.returnValue = "";
+    clearValues();
+})
+//The delete button was deleting its own card only with the default one
+//This function allow it to delete any card in the main content
+content.addEventListener("click", function(event) {
+    if (event.target.classList.contains('delete')) {
+        const card = event.target.closest('.card');
+        card.remove();
+    }
+});
+const checkbox = document.querySelector('.check')
+
+checkbox.addEventListener("click", () =>{
+    if (cardReadStatus.textContent == "Not Read ") {
+        cardReadStatus.textContent = "Read "
+    } else if (cardReadStatus.textContent === "Read "){
+        cardReadStatus.textContent = "Not Read "
+    }
+})
